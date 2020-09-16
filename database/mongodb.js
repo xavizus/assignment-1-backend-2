4d1f49482d48c77dc3e1e77dfb5170aa1277c6e2
Stephan Ljungros <stephan.ljungros@gmail.com>
2020-09-16 12:17:13 +0200

    Added mongodb-in-memory for testing.

--- database/mongodb.js
+++ database/mongodb.js
@@ -3,14 +3,35 @@
  */
 
 const mongoose = require('mongoose');
-let auth = '';
-if (process.env.DBUSER && process.env.DBPASSWORD) {
-    auth = `${process.env.DBUSER}:${process.env.DBPASSWORD}@`
+
+let mongodb;
+
+switch(process.env.ENVIRONMENT) {
+    case "TEST":
+        const {MongoMemoryServer} = require('mongodb-memory-server');
+        mongodb = new MongoMemoryServer();
+        break;
+    case "DEVELOPMENT":
+    case "PRODUCTION":
+    case "STAGING":
+        let auth = '';
+        if (process.env.DBUSER && process.env.DBPASSWORD) {
+            auth = `${process.env.DBUSER}:${process.env.DBPASSWORD}@`
+        }
+        let tls = (process.env.DBTLS.toLowerCase() == 'true')? '+srv' : ''
+        mongodb = {
+            getUri: async () =>
+                `mongodb${tls}://${auth}${process.env.DBHOST}:/${process.env.DATABASE}`
+        }
+        break;
+    default:
+        throw new Error(`${process.env.ENVIRONMENT} is not a valid environment!`)
 }
-let tls = (process.env.DBTLS.toLowerCase() == 'true')? '+srv' : ''
-const uri = `mongodb${tls}://${auth}${process.env.DBHOST}:/${process.env.DATABASE}`;
+
+
 
 async function connect() {
+    let uri = await mongodb.getUri();
     await mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
     if (!mongoose.connection) {
         throw new MongooseError("Could not connect to database!");
@@ -21,7 +42,10 @@ async function disconnect() {
     try {
         await mongoose.connection.close(() => {
             console.log('Database connection closed')
-        })
+        });
+        if(process.env.ENVIRONMENT == 'TEST'){
+            await mongodb.stop()
+        }
     } catch (error) {
         console.error(error)
     }
